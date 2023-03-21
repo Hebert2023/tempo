024e3e8222b0410ab60d49a579f3fa8989f03fd2
hebert2023 <hebertalvescarvalho@gmail.com>
2023-03-20 21:40:21 -0300

    primeiro

---
+++ srcipts.js
@@ -0,0 +1,26 @@
+let chave = "cebcd482eda57fa9a6714c1c2ba91885"
+
+function colocanatela(dados) {
+    console.log(dados)
+
+    document.querySelector(".cidade").innerHTML = "Tempo Em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".descricao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML ="Umidade : "+(dados.main.humidity) + "%"
+    document.querySelector(".icone").src="https://openweathermap.org/img/wn/"+dados.weather[0].icon+".png"
+}
+async function buscarcidade(cidade) {
+    let dados = await fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q="
+        + cidade + "&appid=" + chave + "&lang=pt_br" + "&units=metric")
+        .then(resposta => resposta.json())
+
+    colocanatela(dados)
+
+}
+
+function cliqueinobotao() {
+    let cidade = document.querySelector(".input-cidade").value
+
+    buscarcidade(cidade)
+}
